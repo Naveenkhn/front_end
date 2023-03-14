ed755513142b7d8b203ff5f09627fe794e428534
Naveenkhn <101249457+Naveenkhn@users.noreply.github.com>
2023-03-14 11:34:52 +0530

    Create gen_random.js

---
+++ gen_random.js
@@ -0,0 +1,5 @@
+// Math.random outputs any number between 0 and 0.99
+// Math.ceil rounds off a decimal number in upward direction.
+
+for (i=0; i<10; i++)
+    console.log(Math.ceil(Math.random()*10));
